ae98b70a8fe10a78557777d1de4d2c715cb33fcc
JP094 <46525717+Jenish094@users.noreply.github.com>
2025-07-20 02:50:19 +0930

    Update drive.cpp

--- src/drive.cpp
+++ src/drive.cpp
@@ -4,14 +4,14 @@
 
 using namespace vex;
 
-void driveStraight(double distanceInInches, double speedPercent) {
+void drive(double distanceInInches, double speedPercent) {
   FrontLmotor.spinFor(forward, distanceInInches, inches, speedPercent, percent, false);
   BackLmotor.spinFor(forward, distanceInInches, inches, speedPercent, percent, false);
   FrontRmotor.spinFor(forward, distanceInInches, inches, speedPercent, percent, false);
   BackRmotor.spinFor(forward, distanceInInches, inches, speedPercent, percent);
 }
 
-void turnInPlace(double distanceInInches, double speedPercent) {
+void turn(double distanceInInches, double speedPercent) {
   // Left side reverse, right side forward
   FrontLmotor.spinFor(reverse, distanceInInches, inches, speedPercent, percent, false);
   BackLmotor.spinFor(reverse, distanceInInches, inches, speedPercent, percent, false);
