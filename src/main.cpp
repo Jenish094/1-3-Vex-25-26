442321e15d5acfdb54252f363de8511fdeb3d8c1
JP094 <46525717+Jenish094@users.noreply.github.com>
2025-07-20 02:24:26 +0930

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,33 +9,12 @@
 // Were cooked
 // This is a Pre-Season starter template, this controls the 4-motor drivetrain
 #include "vex.h"
+#include "devices.h"
 
 using namespace vex;
 
 // A global instance of competition
 competition Competition;
-
-/*---------------------------------------------------------------------------*/
-/*                          Defining Robot Devices                           */
-/*---------------------------------------------------------------------------*/
-
-brain Brain;
-controller Controller1;
-
-// Here just change the port number to match the actual robot
-// Drivetrain
-motor FrontLmotor(PORT2, ratio6_1, false); //front left motor
-motor BackLmotor(PORT1, ratio6_1, false); // back left motor
-motor FrontRmotor(PORT3, ratio6_1, true); // reversed- front right motor
-motor BackRmotor(PORT4, ratio6_1, true);  // reversed- back right motor
-// 6:1 ratio was used to accellerate speed as our preseason theories lead to a fast-paced game
-
-// Other devices
-motor IntakeMotor(PORT5, ratio6_1, true); // intake motor
-motor IntakeMotor2(PORT6, ratio6_1, false); // second intake motor
-motor_group IntakeWheels(IntakeMotor, IntakeMotor2 ); // intake wheels motor group
-
-motor Chain(PORT7, ratio6_1, false); //Chain
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
