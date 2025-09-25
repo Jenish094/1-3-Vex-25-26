958d2bc75e1b8f499875b79e633a1ffd5a68127f
JP094 <prakashjenish3@gmail.com>
2025-09-25 23:16:51 +0930

    readded grafana, need to do changes on linux to complete it.

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,9 @@
 #include "main.h"
+#include "pros-grafana-lib/api.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -37,57 +14,41 @@ void initialize() {
 void disabled() {}
 
 /**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
+Runs After initialize() but before autonomous while cdonnected to the field
  */
-void competition_initialize() {}
+void competition_initialize() {
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
+}
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
+void autonomous() {
+
+}
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	Controller master(E_CONTROLLER_MASTER);
 
+	// init grafana
+	grafanalib::GUIManager manager;
+	manager.setRefreshRate(20);
+	// MOTORS
+	Motor LF(1);
+	Motor RF(2);
+	Motor LB(3);
+	Motor RB(4);
+
+	grafanalib::Variable<Motor> leftFrontMotorVar("Left Front Motor", LF);
+	grafanalib::Variable<Motor> rightFrontMotorVar("Right Front Motor", RF);
+	grafanalib::Variable<Motor> leftBackMotorVar("Left Back Motor", LB);
+	grafanalib::Variable<Motor> rightBackMotorVar("Right Back Motor", RB);
+	grafanalib::VariableGroup<Motor> chassisVars({leftFrontMotorVar, rightFrontMotorVar, leftBackMotorVar, rightBackMotorVar});
+	chassisVars.add_getter("Temperature", &Motor::get_temperature);
+	chassisVars.add_getter("Actual Velocity", &Motor::get_actual_velocity);
+	chassisVars.add_getter("Voltage", &Motor::get_voltage);
+	chassisVars.add_getter("Efficiency", &Motor::get_efficiency);
+	manager.registerDataHandler(&chassisVars);
+	manager.startTask();
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		left_mtr = left;
-		right_mtr = right;
-
-		pros::delay(20);
+		delay(20);
 	}
+
 }
